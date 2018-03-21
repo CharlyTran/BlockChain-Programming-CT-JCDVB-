cb9eabae95ee2e1b6503b8480fe415dfd2924e2b
liriose <liriose@hotmail.fr>
2018-03-21 18:50:46 +0100

    Update tp1.sol

--- tp1.sol
+++ tp1.sol
@@ -15,26 +15,42 @@ contract Referendum {
     mapping (address => Vote) votes;
     Result result;
     string public subject;
+    uint public End ;
     address owner;
+    
+     function Set_End(uint end) public isowner(){
+       End=block.number+end;
+   }
 
-    function Referendum(string _subject) {
+     modifier isowner(){//call for identification
+       if(msg.sender!=owner){
+           throw;
+       }
+       _;
+    }
+       
+
+
+    function Referendum(string _subject,uint _End) {
         subject = _subject;
         owner = msg.sender;
         result = Result(0, 0, 0);
+        End=block.number+_End;
     }
 
     event HasVoted(address voter);
 
-    function vote(bool answer) private returns (bool) {
+    function vote(bool answer) private returns (bool) {//allow to vote
         address voter = msg.sender;
-        if(votes[voter].hasVoted) return false;
+        if(votes[voter].hasVoted) return false;//impossible to vote if already voted
         else {
+            if(block.number>End) return false;//impossible after date exprimed in blocks
             if(answer) result.yes++;
             else result.no++;
             result.number++;
             votes[voter].answer = answer;
             votes[voter].hasVoted = true;
-            HasVoted(voter);
+            HasVoted(voter);//block the possibility to alter your answer
             return true;
         }
     }
@@ -50,9 +66,12 @@ contract Referendum {
     function getResult() public constant returns(uint, uint, uint) {
         return (result.yes, result.no, result.number);
     }
+    
+    
 
-    function kill() {
+    function Cloture() {//aka fonction kill
         if(owner == msg.sender) {
+            getResult();
             suicide(owner);
         }
     }
